dd84616540d9877af278192b583c3eddff68a4be
Takahiro Nakamura <dxbxdxbxdxb+dev@gmail.com>
2018-09-22 13:29:32 +0900

    try menubar

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 const { app, BrowserWindow, Menu } = require("electron");
+const menubar = require("menubar");
 
-const menuTemplate = [
-  {
-    label: "Edit",
-    submenu: [{ role: "undo" }, { role: "redo" }]
-  },
-  {
-    label: "Window",
-    submenu: [{ role: "minimize" }, { role: "close" }]
-  }
-];
-
-let win;
-const createWindow = () => {
-  win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadFile("index.html");
-  win.webContents.openDevTools();
-  win.on("closed", () => {
-    win = null;
-  });
-  const menu = Menu.buildFromTemplate(menuTemplate);
-  Menu.setApplicationMenu(menu);
-};
-
-app.on("ready", createWindow);
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+const mb = menubar({
+  showDockIcon: true,
+  tooltip: "My first menubar",
+  alwaysOnTop: true
 });
-app.on("activate", () => {
-  if (win === null) {
-    createWindow();
-  }
+
+mb.on("ready", () => {
+  console.log("app is ready");
+});
+
+mb.on("create-window", () => {
+  console.log("create wiondow");
+});
+
+mb.on("show", () => {
+  console.log("open menu");
+});
+
+mb.on("after-create-window", () => {
+  mb.window.openDevTools();
 });
